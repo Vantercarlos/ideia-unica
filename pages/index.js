a8b7412b35d32cb4497e6b089d6cc9dd99f63645
vanter carlos <vantercarlosti@gmail.com>
2021-02-09 21:37:54 -0400

    segunda atualização

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,43 @@
-import {useState} from 'react'
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
+import Image from 'next/image';
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import fetchFromCMS from '../lib/service';
 
-function Home(){
-    return <div>
-                <h3>Home</h3>
-                <Contador />
+export default function Home({ portfolioItems }) {
+  return (
+    <Layout>
+      <div className="entries">
+        <div className="row justify-content-start  ">
+          {portfolioItems.map((portfolio) => (
+            <div className="col-md-6">
+              <div className="entry mb-3">
+                <Link as={`/portfolio/${portfolio.slug}`} href="/portfolio/[id]">
+                  <div className="main-image">
+                    <Image
+                      src={portfolio.image.name}
+                      width={600}
+                      height={400}
+                      alt={portfolio.Headline}
+                    />
+                    <h1>{portfolio.Headline}</h1>
+                  </div>
+                </Link>
+              </div>
             </div>
-}
-
-function Contador(){
-    const [contador,setContador]= useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
+          ))}
         </div>
-    )
+      </div>
+    </Layout>
+  );
 }
 
-export default Home
+export async function getStaticProps() {
+  const portfolioItems = await fetchFromCMS('portfolios');
+  console.log(portfolioItems);
+  return {
+    props: { portfolioItems },
+    revalidate: 1,
+  };
+}
